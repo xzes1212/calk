4b2056c3e1c6febb8557a7308579692db24807c3
xzes1212 <124681183+xzes1212@users.noreply.github.com>
2023-04-18 00:35:00 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,35 @@
-// получаем все кнопки и экран
-const buttons = document.querySelectorAll('.button');
-const screen = document.querySelector('.output');
+let outputScreen = document
+	.getElementById("outputscreen")
+	function display(num){
+		outputScreen.value += num
+	}
+	function Calculate(){
+	try{
+		outputScreen.value = eval
+		(outputScreen.value);
+	}
 
-// обрабатываем клик на любой кнопке
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    // получаем текст кнопки
-    const buttonText = button.textContent;
-    
-    // если текст равен "=", то вычисляем результат
-    if (buttonText === '=') {
-      try {
-        // вычисляем математическое выражение, введенное на экране
-        const result = eval(screen.textContent);
-        // выводим результат на экран
-        screen.textContent = result;
-      } catch (error) {
-        // если в выражении есть ошибка, выводим ее на экран
-        screen.textContent = 'Error';
-      }
-    } else if (buttonText === 'C') {
-      // если текст равен "C", то очищаем экран
-      screen.textContent = '';
-    } else {
-      // добавляем текст кнопки на экран
-      screen.textContent += buttonText;
-    }
-  });
-});
+	catch(err)
+	{
+		alert("Invalid")
+	}
+}
+function Clear(){
+	outputScreen.value = "";
+}
+function del(){
+	outputScreen.value = outputScreen.value
+	.slice(0,-1)
+}
+
+function percent(){
+	try{
+		outputScreen.value = eval
+		(outputScreen.value)/100;
+	}
+
+	catch(err)
+	{
+		alert("Invalid")
+	}
+}
